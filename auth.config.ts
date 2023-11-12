fd40f3145c00e9e0326eefb0cd4163ddcc2ea0a1
Emanuel Peire <63935846+emapeire@users.noreply.github.com>
2023-11-12 15:01:04 -0300

    Update auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -1,19 +1,26 @@
-import type { NextAuthConfig } from "next-auth";
+import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
-  providers: [],
   pages: {
-    signIn: "/login",
+    signIn: '/login',
   },
+  providers: [
+    // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
+    // while this file is also used in non-Node.js environments
+  ],
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
-      if (isOnDashboard) {
+      const protectedPaths = ['/dashboard', '/customers', '/invoices'];
+      const isProtectedPath = protectedPaths.some((path) =>
+        nextUrl.pathname.startsWith(path),
+      );
+
+      if (isProtectedPath) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL("/dashboard", nextUrl));
+        return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
     },
