d2bf5664212e5dca9f7cd8e6920da5ca4ce2900f
Emanuel Peire <emapeire@outlook.com>
2023-11-16 13:24:45 -0300

    Refactor auth configuration and middleware

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,8 @@
-import type { NextAuthConfig } from 'next-auth';
+import type { NextAuthConfig } from 'next-auth'
 
 export const authConfig = {
   pages: {
-    signIn: '/login',
+    signIn: '/login'
   },
   providers: [
     // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
@@ -10,19 +10,15 @@ export const authConfig = {
   ],
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const protectedPaths = ['/dashboard', '/customers', '/invoices'];
-      const isProtectedPath = protectedPaths.some((path) =>
-        nextUrl.pathname.startsWith(path),
-      );
-
-      if (isProtectedPath) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
+      const isLoggedIn = !!auth?.user
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard')
+      if (isOnDashboard) {
+        if (isLoggedIn) return true
+        return false // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+        return Response.redirect(new URL('/dashboard', nextUrl))
       }
-      return true;
-    },
-  },
-} satisfies NextAuthConfig;
+      return true
+    }
+  }
+} satisfies NextAuthConfig
--- auth.ts
+++ auth.ts
@@ -1,18 +1,18 @@
-import NextAuth from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import { authConfig } from "./auth.config";
-import { sql } from "@vercel/postgres";
-import { z } from "zod";
-import type { User } from "@/app/lib/definitions";
-import bcrypt from "bcrypt";
+import NextAuth from 'next-auth'
+import Credentials from 'next-auth/providers/credentials'
+import bcrypt from 'bcrypt'
+import { sql } from '@vercel/postgres'
+import { z } from 'zod'
+import type { User } from '@/app/lib/definitions'
+import { authConfig } from './auth.config'
 
 async function getUser(email: string): Promise<User | undefined> {
   try {
-    const user = await sql<User>`SELECT * from USERS where email=${email}`;
-    return user.rows[0];
+    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`
+    return user.rows[0]
   } catch (error) {
-    console.error("Failed to fetch user:", error);
-    throw new Error("Failed to fetch user.");
+    console.error('Failed to fetch user:', error)
+    throw new Error('Failed to fetch user.')
   }
 }
 
@@ -23,21 +23,21 @@ export const { auth, signIn, signOut } = NextAuth({
       async authorize(credentials) {
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
-          .safeParse(credentials);
+          .safeParse(credentials)
 
         if (parsedCredentials.success) {
-          const { email, password } = parsedCredentials.data;
-          const user = await getUser(email);
-          if (!user) return null;
-          const passwordsMatch = await bcrypt.compare(password, user.password);
+          const { email, password } = parsedCredentials.data
 
-          if (passwordsMatch) return user;
+          const user = await getUser(email)
+          if (!user) return null
+
+          const passwordsMatch = await bcrypt.compare(password, user.password)
+          if (passwordsMatch) return user
         }
 
-        console.log("Invalid credentials");
-
-        return null;
-      },
-    }),
-  ],
-});
+        console.log('Invalid credentials')
+        return null
+      }
+    })
+  ]
+})
